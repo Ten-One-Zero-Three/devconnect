2d59714493b82974ea9900ad46bac448664694ca
An Lam <82059787+anlqt89@users.noreply.github.com>
2026-04-27 08:41:34 -0500

    copy files (#31)
    Co-authored-by: azeemtechdev <azeemibrahim002@gmail.com>

---
+++ client/src/pages/Profile copy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Profile(){
+  return (
+    <section>
+      <h2>Profile</h2>
+      <div className="card">
+        <p>User profile and activity will be shown here.</p>
+      </div>
+    </section>
+  )
+}
